b27d665cabed2f5f41929496c4a63cafbed1f4ee
JaidenSearle <jsearle6@msudenver.edu>
2025-09-24 19:31:52 -0600

    AST Class Setup

--- src/parser.hpp
+++ src/parser.hpp
@@ -4,6 +4,29 @@
 #include <string>
 #include "tokenizer.hpp"
 
+enum class NodeType
+{
+Program, 
+Declaration, 
+Identifier, 
+BinOp, 
+Literal
+
+};
+
+struct ASTNode
+{
+NodeType type;
+std::string value;
+std::vector<ASTNode*> children;
+
+
+};
+ASTNode* MakeNode(NodeType type, const std::string &value = "")
+{
+return new ASTNode{type, value, {}};
+
+};
 
 
 class parser{
@@ -11,7 +34,7 @@ class parser{
 public:
 inline explicit parser(std::vector<token> tokens) : m_tokens(std::move(tokens)) {}
 
-void parse_program()
+ASTNode* parse_program()
 {
     size_t i = 0;
     while(i < m_tokens.size())
@@ -33,42 +56,44 @@ void parse_program()
 
 private:
 
-bool parse_declaration(size_t& i){ 
+ASTNode* parse_declaration(size_t& i)
+{ 
     if(i >= m_tokens.size() || m_tokens[i].type != tokenType::type_keyword)
     {
         std::cout << " >>> expected a type identifier before variable declaration" << std::endl;
-        return false;
+        return nullptr;
     }
 
-    else if(m_tokens[i].value == "int" || m_tokens[i].value == "str"){
+    else if(m_tokens[i].value == "int" || m_tokens[i].value == "str")
+    {
         i++;
-
+        //build type identifier node
         if(i >= m_tokens.size() || m_tokens[i].type != tokenType::identifier)
         {
             std::cout << " >>> expected an identifier" << std::endl;
-            return false;
+            return nullptr;
         }
         i++;
-
+        //build node if identifier is found
         if(i >= m_tokens.size() || m_tokens[i].type != tokenType::op)
         {
             std::cout <<" >>> expected an '=' after variable declaration" << std::endl;
-            return false;
+            return nullptr;
         }
         i++;
-
+        //build node if operator is found
         if(i >= m_tokens.size() || m_tokens[i].type != tokenType::identifier && m_tokens[i].type != tokenType::literal && m_tokens[i].type != tokenType::constant)
         {
             std::cout << " >>> variable declarations must be initilized with a value" << std::endl;
-            return false;
+            return nullptr;
         }
         i++;
-
+        //build node if variable is initilized
         if( i >= m_tokens.size() || m_tokens[i].type != tokenType::newline)
         {
 
             std::cout << " >>> cannot have declarations on the same line" <<  std::endl;
-            return false;
+            return nullptr;
             
         }
         i++;
@@ -81,33 +106,33 @@ bool parse_declaration(size_t& i){
         if(i >= m_tokens.size() || m_tokens[i].type != tokenType::identifier)
         {
             std::cout << " >>> expected an identifier" << std::endl;
-            return false;
+            return nullptr;
         }
         i++;
 
         if(i >= m_tokens.size() || m_tokens[i].type != tokenType::op)
         {
             std::cout <<" >>> expected an '=' after variable declaration" << std::endl;
-            return false;
+            return nullptr;
         }
         i++;
         if(i >= m_tokens.size() || m_tokens[i].type != tokenType::constant)
         {
             std::cout << "expected a value" << std::endl;
-            return false;
+            return nullptr;
         }
         i++;
         if(i >= m_tokens.size() || m_tokens[i].type != tokenType::punctuator)
         {
             std::cout << "value mismatch floats and doubles must not be a whole number" << std::endl;
-            return false;
+            return nullptr;
 
         }
         i++;
         if(i >= m_tokens.size() || m_tokens[i].type != tokenType::constant)
         {
             std::cout << "expected a value after the decimal" << std::endl;
-            return false;
+            return nullptr;
 
         }
         i++;
@@ -117,15 +142,18 @@ bool parse_declaration(size_t& i){
     else if(m_tokens[i].value == "arr")
     {
         i++;
-        
+        //TODO: add array handling
 
     }
 
      std::cout << " >>> parse sucessful" << std::endl;
-     return true;
 
     }
+    ASTNode* parse_expression(size_t& i)
+    {
 
+
+    }
     const std::vector<token> m_tokens;
     size_t m_index = 0;
 
--- src/tokenizer.hpp
+++ src/tokenizer.hpp
@@ -7,7 +7,8 @@
 #include <iostream>
 #include <cctype>
 
-enum class tokenType{
+enum class tokenType
+{
    identifier,
    type_keyword,
    op_keyword,
@@ -19,74 +20,88 @@ enum class tokenType{
 
 };
 
-struct token{
+struct token
+{
 tokenType type;
 std::optional<std::string> value;
 std::optional<int> line;
 
 };
 
-class tokenizer{
+class tokenizer
+{
 
 public:
 
     inline tokenizer(const std::string& str) : m_str(str) {}
 
 
-    inline std::vector<token> tokenize(){
+    inline std::vector<token> tokenize()
+    {
         std::vector<token> tokens;
         size_t i = m_index;
         const size_t n = m_str.size();
 
-        while (i < n) {
+        while (i < n) 
+        {
             char c = m_str[i];
 
-            if (c == '\n'){
+            if (c == '\n')
+            {
                 tokens.push_back({tokenType::newline, "\\n", m_line});
                 m_line++;
                 i++;
                 continue;
             }
 
-            if(std::isspace(static_cast<unsigned char>(c))){
+            if(std::isspace(static_cast<unsigned char>(c)))
+            {
                 i++;
                 continue;
             }
 
         
-            if (std::isalpha(static_cast<unsigned char>(c)) || c == '_') {
+            if (std::isalpha(static_cast<unsigned char>(c)) || c == '_') 
+            {
             std::string buf;
 
-                while (i < n && (std::isalnum(static_cast<unsigned char>(m_str[i])) || m_str[i] == '_')){
+                while (i < n && (std::isalnum(static_cast<unsigned char>(m_str[i])) || m_str[i] == '_'))
+                {
                     buf.push_back(m_str[i]);
                     i++;
                 }
 
-                static const std::unordered_set<std::string> type_keywords = {
+                static const std::unordered_set<std::string> type_keywords = 
+                {
                     "int", "str", "char", "double", "float","arr"
 
                 };
-                static const std::unordered_set<std::string> op_keywords = {
+                static const std::unordered_set<std::string> op_keywords = 
+                {
                     "for", "while", "if", "else"
 
                 };
                 //keywords
-                if(type_keywords.count(buf)){
+                if(type_keywords.count(buf))
+                {
                     tokens.push_back({tokenType::type_keyword, buf, m_line});
 
                 }
-                else{
+                else
+                {
                     tokens.push_back({tokenType::identifier, buf, m_line});
 
                 }
                 continue;
 
-                if(op_keywords.count(buf)){
+                if(op_keywords.count(buf))
+                {
                     tokens.push_back({tokenType::op_keyword, buf, m_line});
 
                 }
                 //identifiers
-                else{
+                else
+                {
                     tokens.push_back({tokenType::identifier, buf, m_line});
 
                 }
@@ -94,10 +109,12 @@ public:
             }
 
             //constant
-            if(std::isdigit(static_cast<unsigned char>(c))){
+            if(std::isdigit(static_cast<unsigned char>(c)))
+            {
                 std::string num;
 
-                while(i < n && std::isdigit(static_cast<unsigned char>(m_str[i]))){
+                while(i < n && std::isdigit(static_cast<unsigned char>(m_str[i])))
+                {
                     num.push_back(m_str[i]);
                     i++;
                 }
@@ -106,14 +123,16 @@ public:
             }
 
             //operators
-            if(std::string("+-*/%=<>^").find(c) != std::string::npos){
+            if(std::string("+-*/%=<>^").find(c) != std::string::npos)
+            {
                 tokens.push_back({tokenType::op, std::string(1, c), m_line}); 
                 i++;
                 continue;
             }
 
             //punctuators
-            if(std::string("!#$&'(){}[];:?-_,.~|@`").find(c) != std::string::npos){
+            if(std::string("!#$&'(){}[];:?-_,.~|@`").find(c) != std::string::npos)
+            {
                 tokens.push_back({tokenType::punctuator, std::string(1, c), m_line});
                 i++;
                 continue;
@@ -122,33 +141,41 @@ public:
             }
 
             //literals
-            if (m_str[i] == '"'){
+            if (m_str[i] == '"')
+            {
                 std::string str_lit;
                 str_lit.push_back(m_str[i]); 
                 i++;
 
-                while (i < n ){
+                while (i < n )
+                {
                     char c = m_str[i];
 
                     if (c == '\\'){
                         
-                        if (i + 1 < n){
+                        if (i + 1 < n)
+                        {
                             str_lit.push_back(c);
                             str_lit.push_back(m_str[i + 1]);
                             i += 2;
-                        } else {
+                        } 
+
+                        else 
+                        {
                             std::cerr << "error: unfinished escape sequence in string literal\n";
                             std::exit(EXIT_FAILURE);
                         }
 
                     } 
-                    else if (c == '"'){
+                    else if (c == '"')
+                    {
                         str_lit.push_back(c); 
                         i++;
                         break;
 
                     } 
-                    else{
+                    else
+                    {
                         str_lit.push_back(c);
                         i++;
                     }
